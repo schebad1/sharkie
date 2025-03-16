0505fe39bb5c7caf14b55acedfef10dbcaae8cc2
Scheker Badjaj <schebad@mail.de>
2025-03-16 15:28:17 +0100

    Added Animation for shark

--- models/character.class.js
+++ models/character.class.js
@@ -5,9 +5,31 @@ class Character extends MovableObject {
     y = 80;
     x = 0;
 
+    IMAGES_SWIMMING = [
+        'img/1.Sharkie/1.IDLE/1.png',
+        'img/1.Sharkie/3.Swim/1.png',
+        'img/1.Sharkie/3.Swim/2.png',
+        'img/1.Sharkie/3.Swim/3.png',
+        'img/1.Sharkie/3.Swim/4.png',
+        'img/1.Sharkie/3.Swim/5.png',
+        'img/1.Sharkie/3.Swim/6.png',
+    ];
+    currentImage = 0;
 
     constructor() {
         super().loadImage('img/1.Sharkie/1.IDLE/1.png')
+        this.loadImages(this.IMAGES_SWIMMING);
+
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_SWIMMING.length;
+            let path = this.IMAGES_SWIMMING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 200);
     }
 
     jump() {
