dd27f96a26e3c797b12b665102ca1a3a69f0faa3
Scheker Badjaj <schebad@mail.de>
2025-03-17 17:29:19 +0100

    implement camera movement and new background images

--- models/character.class.js
+++ models/character.class.js
@@ -30,13 +30,13 @@ class Character extends MovableObject {
                 this.x += this.speed;
                 this.otherDirection = false;
             }
-        }, 1000 / 60);
 
-        setInterval(() => {
             if(this.world.keyboard.LEFT) {
                 this.x -= this.speed;
                 this.otherDirection = true;
             }
+
+            this.world.camera_x = -this.x;
         }, 1000 / 60);
 
         setInterval(() => {
--- models/world.class.js
+++ models/world.class.js
@@ -6,15 +6,38 @@ class World {
         new Pufferfish(),
     ];
     backgroundObjects = [
+        new BackgroundObject('img/3. Background/Layers/5. Water/D2.png', -719),
+        new BackgroundObject('img/3. Background/Legacy/Layers/1. Light/2.png', -719),
+        new BackgroundObject('img/3. Background/Layers/4.Fondo 2/D2.png', -719),
+        new BackgroundObject('img/3. Background/Layers/3.Fondo 1/D2.png', -719),
+        new BackgroundObject('img/3. Background/Layers/2. Floor/D2.png', -719),
+
         new BackgroundObject('img/3. Background/Layers/5. Water/D1.png', 0),
         new BackgroundObject('img/3. Background/Legacy/Layers/1. Light/1.png', 0),
         new BackgroundObject('img/3. Background/Layers/4.Fondo 2/D1.png', 0),
         new BackgroundObject('img/3. Background/Layers/3.Fondo 1/D1.png', 0),
         new BackgroundObject('img/3. Background/Layers/2. Floor/D1.png', 0),
+        new BackgroundObject('img/3. Background/Layers/5. Water/D2.png', 719),
+        new BackgroundObject('img/3. Background/Legacy/Layers/1. Light/2.png', 719),
+        new BackgroundObject('img/3. Background/Layers/4.Fondo 2/D2.png', 719),
+        new BackgroundObject('img/3. Background/Layers/3.Fondo 1/D2.png', 719),
+        new BackgroundObject('img/3. Background/Layers/2. Floor/D2.png', 719),
+
+        new BackgroundObject('img/3. Background/Layers/5. Water/D1.png', 719*2),
+        new BackgroundObject('img/3. Background/Legacy/Layers/1. Light/1.png', 719*2),
+        new BackgroundObject('img/3. Background/Layers/4.Fondo 2/D1.png', 719*2),
+        new BackgroundObject('img/3. Background/Layers/3.Fondo 1/D1.png', 719*2),
+        new BackgroundObject('img/3. Background/Layers/2. Floor/D1.png', 719*2),
+        new BackgroundObject('img/3. Background/Layers/5. Water/D2.png', 719*3),
+        new BackgroundObject('img/3. Background/Legacy/Layers/1. Light/2.png', 719*3),
+        new BackgroundObject('img/3. Background/Layers/4.Fondo 2/D2.png', 719*3),
+        new BackgroundObject('img/3. Background/Layers/3.Fondo 1/D2.png', 719*3),
+        new BackgroundObject('img/3. Background/Layers/2. Floor/D2.png', 719*3),
     ];
     canvas;
     ctx;
     keyboard;
+    camera_x = 0;
 
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
@@ -30,9 +53,14 @@ class World {
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.ctx.translate(this.camera_x, 0);
+
         this.addObjectsToMap(this.backgroundObjects);
         this.addToMap(this.character);
         this.addObjectsToMap(this.enemies);
+
+        this.ctx.translate(-this.camera_x, 0);
 
         self = this;
         requestAnimationFrame(function() {
