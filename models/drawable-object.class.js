25cbdb71a79f310a8299ee32e88363428d4b8943
Scheker Badjaj <schebad@mail.de>
2025-03-20 15:59:20 +0100

    added statusBar for lifepoints

--- models/drawable-object.class.js
+++ models/drawable-object.class.js
@@ -16,6 +16,17 @@ class DrawableObject {
         ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
     }
 
+    drawFrame(ctx) {
+
+        if(this instanceof Character || this instanceof Pufferfish) {
+            ctx.beginPath();
+            ctx.lineWidth = '5';
+            ctx.strokeStyle = 'blue';
+            ctx.rect(this.x, this.y, this.width, this.height);
+            ctx.stroke();
+        }
+    }
+
     loadImages(arr) {
         arr.forEach((path) => {
             let img = new Image();
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,17 +4,6 @@ class MovableObject extends DrawableObject {
     energy = 100;
     lastHit = 0;
 
-    drawFrame(ctx) {
-
-        if(this instanceof Character || this instanceof Pufferfish) {
-            ctx.beginPath();
-            ctx.lineWidth = '5';
-            ctx.strokeStyle = 'blue';
-            ctx.rect(this.x, this.y, this.width, this.height);
-            ctx.stroke();
-        }
-    }
-
     isColliding(object) {
         return (
           this.x + 40 + (this.width - 80) >= object.x &&
