e9c4b8882456b17256e0605384d7d760ad1ed2a7
Scheker Badjaj <schebad@mail.de>
2025-03-21 01:45:15 +0100

    added more pufferfishes

--- levels/level1.js
+++ levels/level1.js
@@ -3,6 +3,10 @@ const level1 = new Level(
         new Pufferfish(),
         new Pufferfish(),
         new Pufferfish(),
+        new Pufferfish(),
+        new Pufferfish(),
+        new Pufferfish(),
+        new Pufferfish(),
         new Endboss(),
     ],
 
--- models/pufferfish.class.js
+++ models/pufferfish.class.js
@@ -1,31 +1,48 @@
 class Pufferfish extends MovableObject {
-
-    height = 90;
-    width = 90;
+    height = 80;
+    width = 80;
+  
     IMAGES_SWIMMING = [
-        'img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png',
-        'img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim2.png',
-        'img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim3.png',
-        'img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim4.png',
-        'img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim5.png',
+      "img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png",
+      "img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim2.png",
+      "img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim3.png",
+      "img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim4.png",
+      "img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim5.png",
     ];
-
+  
     constructor() {
-        super().loadImage('img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png');
-        this.loadImages(this.IMAGES_SWIMMING);
-
-        this.x = 200 + Math.random() * 500;
-        this.y = Math.random() * 500;
-        this.speed = 0.15 + Math.random() * 0.25;
-
-        this.animate();
+      super().loadImage(
+        "img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png"
+      );
+      this.loadImages(this.IMAGES_SWIMMING);
+  
+      this.x = 400 + Math.random() * 2200;
+      this.y = 0 + Math.random() * 220;
+  
+      this.speed = 0.4 + Math.random() * 0.3;
+  
+      this.speedY = 0.5 + Math.random() * 0.3;
+  
+      this.animate();
     }
-
+  
     animate() {
-        this.moveLeft();
-
-        setInterval(() => {
-            this.playAnimation(this.IMAGES_SWIMMING);
-        }, 200);
+      this.moveLeft();
+  
+      setInterval(() => {
+        this.playAnimation(this.IMAGES_SWIMMING);
+  
+        this.y += this.speedY;
+  
+        if (this.y <= -100) {
+          this.y = -100;
+          this.speedY *= -1; 
+        }
+        if (this.y >= 220) {
+          this.y = 220;
+          this.speedY *= -1;
+        }
+      }, 200);
     }
-}
+  }
+  
