9fcfa65d8cc0327f756e21d9430089342c364db4
Scheker Badjaj <schebad@mail.de>
2025-04-03 21:17:51 +0200

    corrected sleeping immages

--- models/character.class.js
+++ models/character.class.js
@@ -52,21 +52,21 @@ class Character extends MovableObject {
   
     IMAGES_SLEEPING = [
       "img/1.Sharkie/2.Long_IDLE/i1.png",
-      "img/1.Sharkie/2.Long_IDLE/i2.png",
-      "img/1.Sharkie/2.Long_IDLE/i3.png",
-      "img/1.Sharkie/2.Long_IDLE/i4.png",
-      "img/1.Sharkie/2.Long_IDLE/i5.png",
-      "img/1.Sharkie/2.Long_IDLE/i6.png",
-      "img/1.Sharkie/2.Long_IDLE/i7.png",
-      "img/1.Sharkie/2.Long_IDLE/i8.png",
-      "img/1.Sharkie/2.Long_IDLE/i9.png",
-      "img/1.Sharkie/2.Long_IDLE/i10.png",
-      "img/1.Sharkie/2.Long_IDLE/i11.png",
-      "img/1.Sharkie/2.Long_IDLE/i12.png",
-      "img/1.Sharkie/2.Long_IDLE/i13.png",
-      "img/1.Sharkie/2.Long_IDLE/i14.png"
+      "img/1.Sharkie/2.Long_IDLE/I2.png",
+      "img/1.Sharkie/2.Long_IDLE/I3.png",
+      "img/1.Sharkie/2.Long_IDLE/I4.png",
+      "img/1.Sharkie/2.Long_IDLE/I5.png",
+      "img/1.Sharkie/2.Long_IDLE/I6.png",
+      "img/1.Sharkie/2.Long_IDLE/I7.png",
+      "img/1.Sharkie/2.Long_IDLE/I8.png",
+      "img/1.Sharkie/2.Long_IDLE/I9.png",
+      "img/1.Sharkie/2.Long_IDLE/I10.png",
+      "img/1.Sharkie/2.Long_IDLE/I11.png",
+      "img/1.Sharkie/2.Long_IDLE/I12.png",
+      "img/1.Sharkie/2.Long_IDLE/I13.png",
+      "img/1.Sharkie/2.Long_IDLE/I14.png"
     ];
-  
+
     IMAGES_FINSLAP = [
       "img/1.Sharkie/4.Attack/Fin slap/1.png",
       "img/1.Sharkie/4.Attack/Fin slap/2.png",
